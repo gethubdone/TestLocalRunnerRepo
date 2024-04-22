ee43c33fc8683d5210aa684e28f52306052a2392
Stephen S. Mitchell <stephen@stephensmitchell.com>
2024-04-22 14:08:13 -0500

    Update Console.WriteLine in Program.vb to output "+6  : ISWORKING?"

--- Program.vb
+++ Program.vb
@@ -2,6 +2,6 @@ Imports System ' Imports the System namespace, making its public classes and mem
 Module Program ' Declares the Program module.
     Sub Main(args As String()) ' Declares the Main subroutine, the entry point of the program, which can accept command-line arguments.
         Console.WriteLine("Hello World!") ' Outputs "Hello World!" to the console.
-        Console.WriteLine("+5") ' Outputs "+5" to the console.
+        Console.WriteLine("+6  : ISWORKING?") ' Outputs "+5" to the console.
     End Sub ' Ends the Main subroutine.
 End Module ' Ends the Program module.

5971e696ba456c5052de252e2dd9929b213d2b9a
Stephen S. Mitchell <stephen@stephensmitchell.com>
2024-04-22 13:07:52 -0500

    Update Console.WriteLine in Program.vb

--- Program.vb
+++ Program.vb
@@ -2,6 +2,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("+1")
+        Console.WriteLine("+2")
     End Sub
 End Module
